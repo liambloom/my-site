7beba6f85a66c14ceb046a3f996cfb2300a30cff
liambloom <liamrbloom@gmail.com>
2020-05-10 01:24:06 -0400

    Navigation almost works
    It works--for the most part. Closing the menu does crash the broser though.

--- js/header.js
+++ js/header.js
@@ -1,36 +1,84 @@
 const openMenuContainer = document.getElementById("open-menu-container");
 
-const closeMenu = event => {
-  console.log("closeMenu");
-  if (!menu.element.contains(event.target) && !openMenuContainer.contains(event.target)) menu.close();
-};
-
 window.menu = {
-  get open() {
-    return  this._transition.bind(this, true);
+  open() {
+    console.log("pre-open");
+    return this._transition(true);
   },
-  get close() {
-    return this._transition.bind(this, false);
+  async close() {
+    console.log("pre-close");
+    console.log(new Error());
+    await this._transition(false);
+    while (history.state.menuDepth) {
+      console.log(history.state.menuDepth);
+      history.back();
+    }
+    menu.depth = 1;
   },
   _transition (forwards) {
-    document[(forwards ? "add" : "remove") + "EventListener"]("click", closeMenu);
-    this.element.classList[forwards ? "add" : "remove"]("open");
-    for (let rect of Array.from(document.getElementById("open-menu").getElementsByTagName("rect"))) {
-      for (let animation of rect.querySelectorAll(`[data-direction="${forwards ? "forwards" : "backwards"}"]`)) {
-        animation.beginElement();
-        setTimeout(animation.endElement.bind(animation), 500);
+    console.log(this.isOpen, forwards);
+    if (this.isOpen === forwards) return Promise.resolve();
+    else return new Promise(resolve => {
+      console.log(forwards ? "open" : "close");
+      menu.element.addEventListener("transitionend", () => resolve());
+      if (this.isOpen && this.depth !== 1) handle(new Error("The error should never be thrown: The menu is open but not at the top level"));
+      document[(forwards ? "add" : "remove") + "EventListener"]("click", menu._closeOnClickOut);
+      this.element.classList[forwards ? "add" : "remove"]("open");
+    });
+  },
+  toggle () {
+    if (this.isOpen) this.close();
+    else this.open();
+  },
+  initSubMenus (subMenu = this.listElement) {
+    for (let li of subMenu.children) {
+      if (li.getElementsByTagName("ul").length) {
+        li.classList.add("menu");
+        li.addEventListener("click", () => {
+          if (menu.isOpen) {
+            menu.depth++;
+            menu.pushDepth();
+          }
+        });
+        this.initSubMenus(li);
       }
     }
   },
-  toggle () {
-    if (this.element.classList.contains("open")) this.close();
-    else this.open();
+  pushDepth () {
+    const state = history.state || {};
+    delete state.menuDepth;
+    if (menu.open) state.menuDepth = menu.depth;
+    history.pushState(state, "");
   },
-  element: document.getElementsByTagName("nav")[0]
+  _closeOnClickOut (event) { 
+    if (!menu.element.contains(event.target)) menu.close();
+  },
+  element: document.getElementsByTagName("nav")[0],
+  get listElement () {
+    return this.element.getElementsByTagName("ul")[0];
+  },
+  _depth: 1,
+  get depth () {
+    return this._depth;
+  },
+  set depth (value) {
+    if (value === this.depth) return;
+    if (value < 1) handle(new RangeError(`Menu depth cannot be set to ${value}, must be 1 or greater`));
+    this._depth = value;
+    menu.listElement.style.right = (menu.depth - 1) * menu.listElement.clientWidth + "px";
+  },
+  get isOpen () {
+    return this.element.classList.contains("open");
+  }
 };
 for (let e in menu) {
   if (typeof menu[e] === "function") menu[e] = menu[e].bind(menu);
 }
+menu.initSubMenus();
 
-openMenuContainer.addEventListener("click", menu.open);
-document.getElementById("close-menu").parentNode.addEventListener("click", menu.close);
+openMenuContainer.addEventListener("click", event => {
+  event.stopPropagation();
+  menu.pushDepth();
+  menu.open();
+});
+document.getElementById("close-menu").parentNode.addEventListener("click", history.back.bind(history));
--- js/page-loader.js
+++ js/page-loader.js
@@ -1,8 +1,12 @@
 let notInitialLoading = false;
 
+window.currentPage = undefined;
+
 function transition (event) {
   event.preventDefault();
-  history.pushState(null, "", this.href);
+  const state = history.state || {};
+  delete state.menuDepth;
+  history.pushState(state, "", this.href);
   loadPage();
 }
 function transitionLinks () {
@@ -18,70 +22,84 @@ function loaded () {
   window.pageLoadState = "complete";
   window.dispatchEvent(new Event("page-loaded"));
 }
-function loadPage () {
+function loadPage () { // : Promise<boolean> -- If a new page was navigated to, returns true, else false
   "use strict";
+  if (window.currentPage === location.href) return Promise.resolve(false);
   window.pageLoadState = "loading";
-  if (notInitialLoading) Modal.loading.open();
+  if (notInitialLoading) Modal.loading.start();
   else notInitialLoading = true;
   document.getElementById("prev-page").innerHTML = document.getElementById("current-page").innerHTML;
   const now = new Date();
-  fetch("/pages" + location.pathname, {
-    method: "POST",
-    body: JSON.stringify({
-      timestamp: now.getTime()
-    })
-  })
-    .then(page => page.text())
-    .then(page => parseHTML(page))
-    .then(page => {
-      const imports = page.getElementsByTagName("imports")[0];
-      if (imports) imports.remove();
-      return { page, imports };
-    })
-    .then(({page, imports}) => Promise.all([
-      new Promise(resolve => { // imports
-        let importPromises = [];
-        if (imports) {
-          for (let e of Array.from(imports.children)) {
-            if (e.tagName === "SCRIPT") { // This is defined as a function in globals, but I don't want to wait everything to be fetched, parsed, and run before I can do anything.
-              const og = e;
-              e = document.createElement("script");
-              for (let attr of og.attributes) e.setAttribute(attr.name, attr.value);
-            }
-            e.setAttribute("data-for-page", location.pathname);
-            importPromises.push(new Promise(resolveImport => {
-              e.addEventListener("load", resolveImport);
-            }));
-            document.head.appendChild(e);
-          }
-        }
-        resolve(Promise.all(importPromises));
-      }),
-      new Promise(resolve => { // page
-        document.getElementById("current-page").innerHTML = page.innerHTML;
-        resolve();
+  return Promise.all([
+    fetch("/pages" + location.pathname, {
+      method: "POST",
+      body: JSON.stringify({
+        timestamp: now.getTime()
       })
-    ]))
-    .then(() => {
-      transitionLinks();
-    })  
-    .catch(err => {
-      if (document.readyState === "complete") handle(err);
-      else document.addEventListener("load", () => handle(err));
     })
-    .finally(() => {
+      .then(page => page.text())
+      .then(page => parseHTML(page))
+      .then(page => {
+        const imports = page.getElementsByTagName("imports")[0];
+        if (imports) imports.remove();
+        return { page, imports };
+      })
+      .then(({ page, imports }) => Promise.all([
+        new Promise(resolve => { // imports
+          let importPromises = [];
+          if (imports) {
+            for (let e of Array.from(imports.children)) {
+              if (e.tagName === "SCRIPT") { // This is defined as a function in globals, but I don't want to wait everything to be fetched, parsed, and run before I can do anything.
+                const og = e;
+                e = document.createElement("script");
+                for (let attr of og.attributes) e.setAttribute(attr.name, attr.value);
+              }
+              e.setAttribute("data-for-page", location.pathname);
+              importPromises.push(new Promise(resolveImport => {
+                e.addEventListener("load", resolveImport);
+              }));
+              document.head.appendChild(e);
+            }
+          }
+          resolve(Promise.all(importPromises));
+        }),
+        new Promise(resolve => { // page
+          document.getElementById("current-page").innerHTML = page.innerHTML;
+          resolve();
+        })
+      ]))
+      .then(() => {
+        transitionLinks();
+      })
+      .catch(err => {
+        if (document.readyState === "complete") handle(err);
+        else document.addEventListener("load", () => handle(err));
+      }),
+    () => {
+      for (let e of Array.from(document.querySelectorAll("[data-for-page]"))) {
+        if (e.getAttribute("data-for-page") !== location.pathname) e.remove();
+      }
+    }
+  ])
+    .then(() => {
+      window.currentPage = location.href;
       if (document.readyState === "complete") loaded();
       else window.addEventListener("load", loaded);
-    });
-
-  // This is run asynchronously at the same time as the page is being fetched
-  for (let e of Array.from(document.querySelectorAll("[data-for-page]"))) {
-    if (e.getAttribute("data-for-page") !== locaiton.pathname) e.remove();
-  }
+      return true;
+    });  
 }
-window.addEventListener("popstate", () => {
-  Modal.open(document.getElementById("loading-modal"));
-  loadPage();
+window.addEventListener("popstate", event => {
+  console.log("popstate");
+  console.log(event);
+  loadPage()
+    .then(() => {
+      if (event.state && event.state.menuDepth) {
+        if (menu.depth !== (event.state && event.state.menuDepth)) {
+          menu.depth = event.state.menuDepth;
+        }
+      }
+      else menu.close();
+    });
 });
 if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", loadPage);
 else loadPage();
