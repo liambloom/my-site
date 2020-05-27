7d2ff3eb311ef42e198e4903bf8f281eef15f255
liambloom <liamrbloom@gmail.com>
2020-05-27 01:27:12 -0400

    Impovements to page loader
    It now has a PageContentLoaded event (similar to the DOMContentLoaded on a actual webpage loading) and can defer script loading

--- js/global/page-loader.js
+++ js/global/page-loader.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let notInitialLoading = false;
-var currentPage, pageLoadState;
+var currentPage;
 
 function transition (event) {
   event.preventDefault();
@@ -20,17 +20,16 @@ var awaitPageLoad = new Promise(resolve => {
   if (document.readyState === "complete") resolve();
   else window.addEventListener("load", resolve);
 });
-function loaded () {
-  Modal.loading.end();
-  window.pageLoadState = "complete";
-  window.dispatchEvent(new Event("page-loaded"));
-}
+var awaitDocumentReady = new Promise(resolve => {
+  if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", resolve);
+  else resolve();
+});
 function loadPage () { // : Promise<boolean> -- If a new page was navigated to, returns true, else false
   if (currentPage === location.href) return menu.updateMenuState().then(() => false);
-  pageLoadState = "loading";
+  window.pageLoadState = "loading";
   if (notInitialLoading) Modal.loading.start();
   else notInitialLoading = true;
-  document.getElementById("prev-page").innerHTML = document.getElementById("current-page").innerHTML;
+  //document.getElementById("prev-page").innerHTML = document.getElementById("current-page").innerHTML;
   return Promise.all([
     fetch("/pages" + location.pathname, {
       method: "POST",
@@ -48,31 +47,63 @@ function loadPage () { // : Promise<boolean> -- If a new page was navigated to, 
       .then(({ page, imports }) => Promise.all([
         new Promise(resolve => { // imports
           let importPromises = [];
+          let deferredPromises = [];
           if (imports) {
             for (let e of Array.from(imports.children)) {
+              let link;
               if (e.tagName === "SCRIPT") { // This is defined as a function in globals, but I don't want to wait everything to be fetched, parsed, and run before I can do anything.
                 const og = e;
                 e = document.createElement("script");
                 for (let attr of og.attributes) e.setAttribute(attr.name, attr.value);
               }
               e.setAttribute("data-for-page", location.pathname);
-              importPromises.push(new Promise(resolveImport => {
-                e.addEventListener("load", resolveImport);
-              }));
-              document.head.appendChild(e);
+              const isDeferred = e.getAttribute("defer") === "" || e.getAttribute("defer") === "true";
+              if (isDeferred) {
+                link = document.createElement("link");
+                link.rel = "preload";
+                link.as = "script";
+                link.href = e.src;
+                document.head.appendChild(link);
+              }
+              const importPromise = () => new Promise((resolveImport, rejectImport) => {
+                e.addEventListener("load", () => {
+                  resolveImport();
+                });
+                e.addEventListener("error", err => {
+                  if (!(err instanceof ErrorEvent)) rejectImport(err);
+                });
+                setTimeout(() => {
+                  rejectImport(new Error("Loading took too long"));
+                }, 15000);
+                document.head.appendChild(e);
+                if (link) link.remove();
+              });
+              if (isDeferred) deferredPromises.push(importPromise);
+              else importPromises.push(importPromise());
             }
           }
-          resolve(Promise.all(importPromises));
+          resolve(
+            Promise.all(importPromises)
+              .then(() => new Promise(resolveInner => {
+                if (window.pageLoadState === "interactive") resolveInner();
+                else window.addEventListener("pagecontentloaded", resolveInner);
+              }))
+              .then(() => Promise.all(deferredPromises.map(fn => fn())))
+          );
         }),
-        new Promise(resolve => { // page
-          document.getElementById("current-page").innerHTML = page.innerHTML;
-          resolve();
-        })
+        awaitDocumentReady
+          .then(() => {
+            main.innerHTML = page.innerHTML;
+          })
+          .then(() => {
+            window.pageLoadState = "interactive";
+            window.dispatchEvent(new Event("pagecontentloaded"));
+          })
       ]))
       .then(awaitPageLoad)
       .then(() => Promise.all([
         transitionLinks, 
-        awaitPageLoad.then(initCustomInputs)
+        initCustomInputs
       ]))
       .catch(err => awaitPageLoad.then(() => handle(err))),
     () => {
@@ -84,13 +115,20 @@ function loadPage () { // : Promise<boolean> -- If a new page was navigated to, 
     .then(awaitPageLoad)
     .then(() => {
       currentPage = location.href;
-      loaded();
+      Modal.loading.end();
+      window.pageLoadState = "complete";
+      window.dispatchEvent(new Event("pageload"));
       history.replaceState(menu.clearedState, "");
       menu.updateMenuState(); // Run asynchronously, NOT awaited
       return true;
     })
     .catch(err => awaitPageLoad.then(() => handle(err)));  
 }
+awaitDocumentReady
+  .then(() => {
+    window.main = document.getElementsByTagName("main")[0];
+  });
+
 window.addEventListener("popstate", loadPage);
 if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", loadPage);
 else loadPage();
