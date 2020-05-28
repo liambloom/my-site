90ef27cdec2ba6bd4d3b08d438a3a16560bfa294
liambloom <liamrbloom@gmail.com>
2020-05-28 15:07:13 -0400

    Made "scroll for more" disapear on scroll

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,9 @@
 main.style.width = `calc(100vw + ${main.offsetWidth - main.clientWidth}px)`;
 document.querySelector("#welcome > div:first-of-type").style.animation = "spread 1.75s ease forwards, glow 3s ease-in-out forwards";
-document.getElementById("scroll-for-more").style.opacity = "1";
+const showScroll = setTimeout(() => {
+  document.getElementById("scroll-for-more").style.opacity = "1";
+}, 2500);
+main.addEventListener("scroll", () => {
+  clearTimeout(showScroll);
+  document.getElementById("scroll-for-more").style.opacity = "0";
+});
