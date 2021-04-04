3d0990cfb2181551cdb0712d3a7c568d5df88f36
liambloom <liamrbloom@gmail.com>
2021-04-04 19:11:30 -0400

    Changed server change detection
    I decided that since I only check for changes when there is a request (because I have autosave), I shouldn't use notify. So instead, I now use SHA-1 to check if template files have been changed

--- server/templates.rs
+++ server/templates.rs
@@ -3,9 +3,9 @@ use std::{sync::Once, ptr, mem};
 use tera::Tera;
 
 #[cfg(debug_assertions)]
-use std::{time::Duration, sync::{RwLock, RwLockReadGuard, Mutex, mpsc::{channel, Receiver}}};
+use std::{sync::{RwLock, RwLockReadGuard, Mutex}, fs::{read_dir, ReadDir, read, read_link}};
 #[cfg(debug_assertions)]
-use notify::{Watcher, RecursiveMode, RecommendedWatcher, DebouncedEvent};
+use sha1::{Sha1, Digest};
 
 fn tera_init() -> Tera {
     Tera::new("templates/**/*").unwrap_exit()
@@ -15,31 +15,50 @@ fn tera_init() -> Tera {
 pub fn tera() -> RwLockReadGuard<'static, Tera> {
     static ONCE: Once = Once::new();
     static mut TERA: *const RwLock<Tera> = ptr::null();
-    static mut RCV: *const Mutex<Receiver<DebouncedEvent>> = ptr::null();
+    static mut HASH: *const Mutex<[u8; 20]> = ptr::null();
 
     unsafe {
         ONCE.call_once(|| {
-            let (tx, rx) = channel();
             TERA = mem::transmute(Box::new(RwLock::new(tera_init())));
-            let mut watcher: RecommendedWatcher = Watcher::new(tx, Duration::from_millis(0)).unwrap_exit();
-            watcher.watch("templates", RecursiveMode::Recursive).unwrap_exit();
-            mem::forget(watcher);
-            RCV = mem::transmute(Box::new(Mutex::new(rx)));
+            HASH = mem::transmute(Box::new(Mutex::new(get_hash())))
         });
 
-        let rcv = (*RCV).lock().unwrap_exit();
-        let mut changed = false;
-        for _ in rcv.try_iter() {
-            changed = true;
-        }
-
-        if changed {
+        let new_hash = get_hash();
+        let mut old_hash = (*HASH).lock().unwrap_exit();
+        if *old_hash != new_hash {
             (*TERA).write().unwrap_exit().full_reload().unwrap_exit();
+            *old_hash = new_hash;
         }
 
-        drop(rcv);
+        drop(old_hash);
 
         (*TERA).read().unwrap_exit()
+    }
+}
+
+#[cfg(debug_assertions)]
+fn get_hash() -> [u8; 20] {
+    let mut hasher = Sha1::new();
+    hash_dir(&mut hasher, read_dir("templates").unwrap_exit());
+    *hasher.finalize().as_mut()
+}
+
+#[cfg(debug_assertions)]
+fn hash_dir(hasher: &mut Sha1, dir: ReadDir) {
+    for entry in dir {
+        let entry = entry.unwrap_exit();
+        hasher.update(entry.file_name().into_string().expect("Failed to convert OsString"));
+        let mut meta = entry.file_type().unwrap_exit();
+        while meta.is_symlink() {
+            meta = read_link(entry.path()).unwrap_exit().metadata().unwrap_exit().file_type();
+        }
+        if meta.is_dir() {
+            hash_dir(hasher, read_dir(entry.path()).unwrap_exit());
+        }
+        else {
+            debug_assert!(meta.is_file());
+            hasher.update(read(entry.path()).unwrap_exit());
+        }
     }
 }
 
